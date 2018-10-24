eb1439aeab108c4cd32d98a89ba7a5dcf20d4147
lenonis <44026463+lenonis@users.noreply.github.com>
2018-10-24 16:29:36 +0800

    Add files via upload

--- work2expand.m
+++ work2expand.m
@@ -1,20 +1,22 @@
-code= fopen('work2.m','r')
-c=textscan(code,'%s','delimiter','\n')
-[char char_num]=fscanf(code,'%c')
-row=c{1}
-row_num=size(row,1)
-code_num=0
-empty_num=0
-explain_num=0
-for i=1:row_num
-    if strcmp(row(i),' ')
-        empty_num=empty_num+1;
-    else 
-        code_num=code_num+1;
-    end    
-end
-for j=1:char_num
-    if char(j)=='%'
-        explain_num=explain_num+1;
+code=fopen('work2.m','r')
+[char char_number]=fscanf(code,'%c')
+r=0 %与代码行数有关的量
+epm=0 %与空行数有关的量
+ex=0  %与注释行数有关的量
+for i=1:char_number
+    t=fscanf(code,'%c',1)
+    if t==char(10)
+        r=r+1;
+    else t=="%"
+         ex=ex+1;
     end
 end
+
+for j=1:r
+    l=fgetl(code)  %将文件转为一行，并去掉换行符
+    if isempty(l)
+        ex=ex+1
+    end
+end
+
+        
